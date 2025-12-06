fe2bceb2403ca6998ac506b92cb68ac1330efd0d
Kiseki 奇跡 <contact@kiseki-miracle.dev>
2025-12-05 20:11:45 -0500

    feat: removed astro icons and replaced it with Unplugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,33 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import icon from "astro-icon";
-
+import Icons from "unplugin-icons/vite";
 import cloudflare from "@astrojs/cloudflare";
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kiseki-miracle.dev",
-	integrations: [mdx(), sitemap(), icon(), react()],
+  site: "https://kiseki-miracle.dev",
+  integrations: [mdx(), sitemap(), react()],
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [
+      tailwindcss(),
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+      Icons({
+        compiler: "astro",
+        autoInstall: true,
+      }),
+    ],
+  },
 
-	output: "server",
-	adapter: cloudflare(),
+  output: "server",
+  adapter: cloudflare(),
 });
